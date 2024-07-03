1f56a6ad7b54b1fb9a6912be490dc42f192ea4a9
Jeremy Chong <chong.jeremy@hotmail.com>
2024-07-03 13:58:51 -0400

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 require('./api/config');
 
 const express = require('express');
+const cors = require('cors');
 const User = require('./api/config');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/register', async (req, res) => {
   let user = new User(req.body);
