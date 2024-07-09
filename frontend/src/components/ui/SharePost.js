a9c35c1266eaff4002a077ca608d466fda9eb482
Jeremy Chong <chong.jeremy@hotmail.com>
2024-07-09 14:58:50 -0400

    added basic functionality for modal

--- frontend/src/components/ui/SharePost.js
+++ frontend/src/components/ui/SharePost.js
@@ -1,10 +1,25 @@
 import {useState} from "react";
 
 export default function SharePost() {
+  const [visibility, setVisiblity] = useState('isNotVisible');
+
+  function adjustVisibility() {
+    if (visibility === "isVisible") {
+      setVisiblity("isNotVisible");
+    }
+    else {
+      setVisiblity("isVisible")
+    }
+  }
 
   return (
     <div>
-      <h1>Share a post</h1>
+      <button onClick={adjustVisibility}>
+        Add posts
+      </button>
+      <div className={visibility}>
+        <h4>MODAL</h4>
+      </div>
     </div>
   );
 }
