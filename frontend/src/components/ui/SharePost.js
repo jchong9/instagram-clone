6b32a6a3ebb7f337a6587a99c30af0ef5e4511b6
Jeremy Chong <chong.jeremy@hotmail.com>
2024-08-08 18:39:33 -0400

    fixed minor syntax

--- frontend/src/components/ui/SharePost.js
+++ frontend/src/components/ui/SharePost.js
@@ -38,16 +38,17 @@ export default function SharePost() {
 
   return (
     <>
-      <button className="btn btn-outline-primary btn-large" onClick={adjustVisibility}>
+      <button className="btn btn-outline-primary btn-large"
+              onClick={() => setVisibility("isVisible")}>
         + Add posts
       </button>
       <div className="modal-backdrop" id={visibility}></div>
       <div className="modal-container" id={visibility}>
         <div className="modal-header">
-          <h4>Start sharing your memories today</h4>
+          <h4>Start sharing posts today</h4>
           <svg xmlns="http://www.w3.org/2000/svg"
                viewBox="0 0 384 512"
-               onClick={adjustVisibility}>
+               onClick={() => setVisibility("isNotVisible")}>
             <path
               d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
           </svg>
@@ -63,7 +64,10 @@ export default function SharePost() {
             />
             <br/>
             <div>
-              <button className="btn btn-outline-primary" onClick={adjustVisibility}>Close</button>
+              <button className="btn btn-outline-primary"
+                      onClick={() => setVisibility("isNotVisible")}>
+                Close
+              </button>
               <button type="submit" className="btn btn-primary">Upload</button>
             </div>
           </form>
