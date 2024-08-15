94c544cd2fd027887549e814965cc453f73f2564
Jeremy Chong <chong.jeremy@hotmail.com>
2024-08-15 19:07:57 -0400

    added search users function for the explore page

---
+++ frontend/src/components/ui/DisplayUserList.js
@@ -0,0 +1,48 @@
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+export default function DisplayUserList(props) {
+  const [allUsers, setAllUsers] = useState([]);
+  const [loadingMsg, setLoadingMsg] = useState("Loading users... 😅");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingMsg("");
+      getUsers();
+    }, 2000)
+  }, []);
+
+  async function getUsers() {
+    const result = await axios.get("http://localhost:5000/get-all-users", {
+      params: {
+        username: props.usernameSearch
+      }
+    });
+    setAllUsers(result.data);
+  }
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <h3 className="m-3">{loadingMsg}</h3>
+      {!allUsers || allUsers.length === 0 ?
+        <div className="m-2">
+          <h5>No matching users... 😔</h5>
+        </div>
+        : allUsers.map((data) => {
+          return (
+            <div key={data._id} className="card mb-5">
+              <div className="card-header">
+                <Link to="/profile" state={{userID: data._id}}>
+                  <h5>@{data.username}</h5>
+                </Link>
+              </div>
+              <div className="card-footer">
+                <p>{data.bio ? data.bio : "No bio yet"}</p>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
--- frontend/src/pages/Explore.js
+++ frontend/src/pages/Explore.js
@@ -1,18 +1,21 @@
 import {useState} from "react";
 import DisplayPost from "../components/ui/DisplayPost";
+import DisplayUserList from "../components/ui/DisplayUserList";
 
 export default function Explore() {
   const [searchInput, setSearchInput] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
   const [formSubmitted, setFormSubmitted] = useState(false);
-  const [seed, setSeed] = useState(1);
+  const [seed1, setSeed1] = useState(1);
+  const [seed2, setSeed2] = useState(1);
   const user = JSON.parse(localStorage.getItem("user"));
 
   function searchPost(e) {
     e.preventDefault();
     setFormSubmitted(true);
     setSearchQuery(searchInput);
-    setSeed(Math.random());
+    setSeed1(Math.random());
+    setSeed2(10 * seed1);
   }
 
   return (
@@ -30,11 +33,17 @@ export default function Explore() {
       {formSubmitted && searchQuery ? (
         <div>
           <h6 className="text-center">Results for: {searchQuery}</h6>
+          <h2>Related users</h2>
+          <DisplayUserList usernameSearch={searchQuery}
+                           key={seed1}
+          />
+          <h2>Related posts</h2>
           <DisplayPost requestURL="get-image-search"
                        id={user._id}
                        search={searchQuery}
                        following={user.following}
-                       key={seed}/>
+                       key={seed2}
+          />
         </div>
       ) : (
         <div>
@@ -42,7 +51,8 @@ export default function Explore() {
                        id={user._id}
                        search=""
                        following={user.following}
-                       key={seed} />
+                       key={seed2}
+          />
         </div>
       )}
     </>
